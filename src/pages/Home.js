2162a45f977f01e0e3bbc7c12a0bad9936484cfc
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2024-02-05 11:40:48 -0500

    ui updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,14 @@ import '../styles/Home.css';
 
 function Home({ addToCart }) {
   const products = [
-    { id: 1, name: 'Product 1', price: 10, image: 'product1.jpg' },
-    { id: 2, name: 'Product 2', price: 20, image: 'product2.jpg' },
-    { id: 3, name: 'Product 3', price: 15, image: 'product3.jpg' },
-    { id: 4, name: 'Product 4', price: 10, image: 'product4.jpg' },
-    { id: 5, name: 'Product 5', price: 20, image: 'product5.jpg' },
-    { id: 3, name: 'Product 3', price: 15, image: 'product3.jpg' },
-    { id: 4, name: 'Product 4', price: 10, image: 'product4.jpg' },
-    { id: 5, name: 'Product 5', price: 20, image: 'product5.jpg' },
+    { id: 1, name: 'Product 1', price: 10, image: 'images/p.jpg' },
+    { id: 2, name: 'Product 2', price: 20, image: 'images/p-2.jpg' },
+    { id: 3, name: 'Product 3', price: 15, image: 'images/p-1.jpg' },
+    { id: 4, name: 'Product 4', price: 10, image: 'images/p-1.jpg' },
+    { id: 5, name: 'Product 5', price: 20, image: 'images/p-1.jpg' },
+    { id: 3, name: 'Product 6', price: 15, image: 'images/p-1.jpg' },
+    { id: 4, name: 'Product 7', price: 10, image: 'images/p-1.jpg' },
+    { id: 5, name: 'Product 8', price: 20, image: 'images/p-1.jpg' },
   ];
 
   const productsRow1 = products.slice(0, Math.ceil(products.length / 2));
