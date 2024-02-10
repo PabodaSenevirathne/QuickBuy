9de713384db19f959988fa03cf27caaf7c16e798
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2024-02-10 13:33:34 -0500

    images added

--- src/utils/ProductInfo.js
+++ src/utils/ProductInfo.js
@@ -1,12 +1,12 @@
 const products = [
-    { id: 1, name: 'Product 1', price: 10, image: 'images/p-1.jpg' },
-    { id: 2, name: 'Product 2', price: 20, image: 'images/p-2.jpg' },
-    { id: 3, name: 'Product 3', price: 15, image: 'images/p-3.jpg' },
-    { id: 4, name: 'Product 4', price: 10, image: 'images/p-4.jpg' },
-    { id: 5, name: 'Product 5', price: 20, image: 'images/p-5.jpg' },
-    { id: 6, name: 'Product 6', price: 15, image: 'images/p-6.jpg' },
-    { id: 7, name: 'Product 7', price: 10, image: 'images/p-7.jpg' },
-    { id: 8, name: 'Product 8', price: 20, image: 'images/p-8.jpg' },
+    { id: 1, name: 'Sneakers', price: 10, image: require('../images/p-1.jpg') },
+    { id: 2, name: 'Timberland Men', price: 20, image: require('../images/p-2.jpg') },
+    { id: 3, name: 'Nunn Bush', price: 15, image: require('../images/p-3.jpg') },
+    { id: 4, name: 'Vince Camuto', price: 10, image: require('../images/p-4.jpg') },
+    { id: 5, name: 'Aldo', price: 20, image: require('../images/p-5.jpg') },
+    { id: 6, name: 'Clarks Mens', price: 15, image: require('../images/p-6.jpg') },
+    { id: 7, name: 'Elements', price: 10, image: require('../images/p-7.jpg') },
+    { id: 8, name: 'Chelsea Boots', price: 20, image: require('../images/p-8.jpg') },
 ];
 
 
